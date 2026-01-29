47d9fea97296c3ef7f2a4a5324bb03180c8ebd2a
TuananhDo <TuananhDo0308@gmail.com>
2026-01-29 11:36:16 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+const repo = "TuananhDo_Portfolio";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // Tạm thời tắt để test
-  
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
