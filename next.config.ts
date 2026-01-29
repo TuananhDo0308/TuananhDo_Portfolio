b67ddb48d4bdd8a9bfa456a58e31d68a9fafe9c1
TuananhDo <TuananhDo0308@gmail.com>
2026-01-29 11:14:54 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: false,
+    output: "export",              // BẮT BUỘC cho GitHub Pages
+
   basePath: isProd ? "/TuananhDo_Portfolio" : "",
   assetPrefix: isProd ? "/TuananhDo_Portfolio/" : "",
+   images: {
+    unoptimized: true,           // cần cho static export
+  },
 };
 
 module.exports = nextConfig;
