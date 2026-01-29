bdd6d0d290510a2230441e780df28f555e28050c
TuananhDo <TuananhDo0308@gmail.com>
2026-01-29 14:41:13 +0700

    Updates

--- _next/static/I5Vp3uR4u8izaZVdZ28eV/_buildManifest.js
+++ _next/static/I5Vp3uR4u8izaZVdZ28eV/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/TuananhDo_Portfolio//_next/:path+",
+        "destination": "/TuananhDo_Portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
